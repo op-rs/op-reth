53f850a855c9e12b5686a6c47c9a77c32e698ed8
Andreas Bigger <abigger87@gmail.com>
2023-02-26 22:26:58 -0700

    compiles

--- crates/rpc/rpc-types/src/eth/transaction/mod.rs
+++ crates/rpc/rpc-types/src/eth/transaction/mod.rs
@@ -111,6 +111,7 @@ impl Transaction {
 
         let (gas_price, max_fee_per_gas) = match signed_tx.tx_type() {
             TxType::Legacy => (Some(U128::from(signed_tx.max_fee_per_gas())), None),
+<<<<<<< HEAD
             TxType::EIP2930 => (Some(U128::from(signed_tx.max_fee_per_gas())), None),
             TxType::EIP1559 => {
                 // the gas price field for EIP1559 is set to `min(tip, gasFeeCap - baseFee) +
@@ -123,6 +124,10 @@ impl Transaction {
 
                 (Some(U128::from(gas_price)), Some(U128::from(signed_tx.max_fee_per_gas())))
             }
+=======
+            TxType::EIP2930 => (None, Some(U128::from(signed_tx.max_fee_per_gas()))),
+            TxType::EIP1559 => (None, Some(U128::from(signed_tx.max_fee_per_gas()))),
+>>>>>>> 1c17ad88 (compiles)
             #[cfg(feature = "optimism")]
             TxType::DEPOSIT => (None, None),
         };
