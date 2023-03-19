cc623d2f74e0d5c827ab1f5d93a24dcc4c2de311
clabby <ben@clab.by>
2023-03-19 16:53:10 -0400

    Resolve conflicts
    Remove `consensus` crate `Cargo.toml`
    
    Reduce diff

--- crates/consensus/common/src/validation.rs
+++ crates/consensus/common/src/validation.rs
@@ -12,8 +12,6 @@ use reth_primitives::{
 use reth_provider::{AccountReader, HeaderProvider, WithdrawalsProvider};
 use std::collections::{hash_map::Entry, HashMap};
 
-use reth_primitives::constants;
-
 #[cfg(feature = "optimism")]
 use reth_primitives::TxDeposit;
 
@@ -70,11 +68,6 @@ pub fn validate_transaction_regarding_header(
     base_fee: Option<u64>,
 ) -> Result<(), ConsensusError> {
     let chain_id = match transaction {
-        #[cfg(feature = "optimism")]
-        Transaction::Deposit(TxDeposit { .. }) => {
-            // TODO: get the chain id
-            None
-        }
         Transaction::Legacy(TxLegacy { chain_id, .. }) => {
             // EIP-155: Simple replay attack protection: https://eips.ethereum.org/EIPS/eip-155
             if chain_spec.fork(Hardfork::SpuriousDragon).active_at_block(at_block_number) &&
@@ -109,6 +102,11 @@ pub fn validate_transaction_regarding_header(
             }
 
             Some(*chain_id)
+        }
+        #[cfg(feature = "optimism")]
+        Transaction::Deposit(TxDeposit { .. }) => {
+            // TODO: I believe the chain id should be None here, but have to confirm.
+            None
         }
     };
     if let Some(chain_id) = chain_id {
--- crates/primitives/src/transaction/mod.rs
+++ crates/primitives/src/transaction/mod.rs
@@ -72,7 +72,7 @@ pub enum Transaction {
     /// transaction, incentivizing miners to include transactions with higher priority fees per
     /// gas.
     Eip1559(TxEip1559),
-    /// Deposit transaction.
+    /// Optimism deposit transaction.
     #[cfg(feature = "optimism")]
     Deposit(TxDeposit),
 }
