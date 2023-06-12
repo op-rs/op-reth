1cc1e7155de0b66ac2752a339be66edbe4a6f038
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-06-12 21:48:03 +0200

    wip: transact() optimism changes

--- crates/interfaces/src/executor.rs
+++ crates/interfaces/src/executor.rs
@@ -46,7 +46,6 @@ pub enum BlockExecutionError {
     CanonicalRevert { inner: String },
     #[error("Transaction error on commit: {inner:?}")]
     CanonicalCommit { inner: String },
-<<<<<<< HEAD
     // === tree errors ===
     // TODO(mattsse): move this to tree error
     #[error("Block hash {block_hash} not found in blockchain tree chain")]
@@ -75,10 +74,4 @@ impl BlockExecutionError {
     pub fn is_fatal(&self) -> bool {
         matches!(self, Self::CanonicalCommit { .. } | Self::CanonicalRevert { .. })
     }
-=======
-    #[error("Transaction error on pipeline status update: {inner:?}")]
-    PipelineStatusUpdate { inner: String },
-    #[error("DB Error during transaction execution: {inner:?}")]
-    DBError { inner: String },
->>>>>>> 6266b9f3 (feat: receipts, l1 cost wip)
 }
