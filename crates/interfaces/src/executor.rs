651731e71de9bbfe481ee3f490875124d93e9031
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-06-17 22:25:28 +0200

    wip: removed receipts changes + improved gas handling

--- crates/interfaces/src/executor.rs
+++ crates/interfaces/src/executor.rs
@@ -67,6 +67,7 @@ pub enum BlockExecutionError {
     #[cfg(feature = "optimism")]
     #[error("Insufficient funds to cover transaction L1 cost: want {want}, have {have}")]
     InsufficientFundsForL1Cost { want: u64, have: u64 },
+<<<<<<< HEAD
 }
 
 impl BlockExecutionError {
@@ -74,4 +75,6 @@ impl BlockExecutionError {
     pub fn is_fatal(&self) -> bool {
         matches!(self, Self::CanonicalCommit { .. } | Self::CanonicalRevert { .. })
     }
+=======
+>>>>>>> 891b841f (wip: removed receipts changes + improved gas handling)
 }
