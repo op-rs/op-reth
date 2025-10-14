c8c1b4f287c8ca145abd2c1c01ed05e0e8f59e5a
itschaindev <jagrutk@protonmail.com>
2025-10-14 12:17:17 +0200

    add external proofs exex to op

--- crates/optimism/bin/src/main.rs
+++ crates/optimism/bin/src/main.rs
@@ -1,12 +1,53 @@
 #![allow(missing_docs, rustdoc::missing_crate_level_docs)]
 
 use clap::Parser;
+use reth_exex_external_proofs::OpProofsExEx;
 use reth_optimism_cli::{chainspec::OpChainSpecParser, Cli};
 use reth_optimism_node::{args::RollupArgs, OpNode};
 use tracing::info;
 
 #[global_allocator]
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
+
+/// The storage DB for proofs history.
+#[derive(Debug, Clone, PartialEq, Eq, clap::ValueEnum)]
+enum ProofsHistoryStorage {
+    /// SQL Lite
+    Sqlite,
+}
+
+impl std::fmt::Display for ProofsHistoryStorage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ProofsHistoryStorage::Sqlite => f.write_str("sqlite"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, clap::Args)]
+#[command(next_help_heading = "Proofs History")]
+struct Args {
+    #[command(flatten)]
+    pub rollup_args: RollupArgs,
+
+    /// If true, initialize external-proofs exex to save and serve trie nodes to provide proofs faster.
+    #[arg(long = "proofs-history")]
+    pub proofs_history: bool,
+
+    /// The storage DB for proofs history.
+    #[arg(long = "proofs-history.storage", default_value_t = ProofsHistoryStorage::Sqlite)]
+    pub proofs_history_storage: ProofsHistoryStorage,
+
+    /// The path to the storage DB for proofs history.
+    #[arg(long = "proofs-history.storage-path")]
+    pub proofs_history_storage_path: Option<String>,
+
+    /// The window to span blocks for proofs history. Value is the number of blocks.
+    /// Default is 1 month of blocks based on 2 seconds block time.
+    /// 30 * 24 * 60 * 60 / 2 = 1_296_000
+    #[arg(long = "proofs-history.window", default_value_t = 1_296_000)]
+    pub proofs_history_window: u64,
+}
 
 fn main() {
     reth_cli_util::sigsegv_handler::install();
@@ -19,10 +60,19 @@ fn main() {
     }
 
     if let Err(err) =
-        Cli::<OpChainSpecParser, RollupArgs>::parse().run(async move |builder, rollup_args| {
+        Cli::<OpChainSpecParser, Args>::parse().run(async move |builder, args| {
             info!(target: "reth::cli", "Launching node");
+
+            let rollup_args = args.rollup_args;
+
             let handle =
-                builder.node(OpNode::new(rollup_args)).launch_with_debug_capabilities().await?;
+                builder
+                    .node(OpNode::new(rollup_args))
+                    .install_exex_if(args.proofs_history, "proofs-history", async move |exex_context| {
+                        let proofs_helper = OpProofsExEx::new(exex_context);
+                        Ok(proofs_helper.run())
+                    })
+                    .launch_with_debug_capabilities().await?;
             handle.node_exit_future.await
         })
     {
