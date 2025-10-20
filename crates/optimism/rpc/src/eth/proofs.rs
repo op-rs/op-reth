e92517335f8d51d9538c23e1b3033199401f918f
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-10-20 14:31:11 +0200

    Fix rebase conflicts

--- crates/optimism/rpc/src/eth/proofs.rs
+++ crates/optimism/rpc/src/eth/proofs.rs
@@ -10,7 +10,7 @@ use jsonrpsee::proc_macros::rpc;
 use jsonrpsee_core::RpcResult;
 use jsonrpsee_types::error::ErrorObject;
 use reth_optimism_trie::{provider::OpProofsStateProviderRef, OpProofsStorage};
-use reth_provider::{BlockIdReader, ProviderError, ProviderResult, StateProviderBox};
+use reth_provider::{BlockIdReader, ProviderError, ProviderResult, StateProofProvider};
 use reth_rpc_api::eth::helpers::FullEthApi;
 use reth_rpc_eth_types::EthApiError;
 
@@ -41,7 +41,10 @@ where
     ErrorObject<'static>: From<Eth::Error>,
     P: OpProofsStorage + Clone + 'static,
 {
-    async fn state_provider(&self, block_id: Option<BlockId>) -> ProviderResult<StateProviderBox> {
+    async fn state_provider(
+        &self,
+        block_id: Option<BlockId>,
+    ) -> ProviderResult<impl StateProofProvider> {
         let block_id = block_id.unwrap_or_default();
         // Check whether the distance to the block exceeds the maximum configured window.
         let block_number = self
@@ -65,18 +68,15 @@ where
                 .map_err(|e| ProviderError::Database(e.into()))?,
         ) else {
             // if no earliest block, db is empty - use historical provider
-            return Ok(historical_provider);
+            return Ok(historical_provider as Box<dyn StateProofProvider>);
         };
 
         if block_number < earliest_block_number || block_number > latest_block_number {
-            return Ok(historical_provider);
+            return Ok(historical_provider as Box<dyn StateProofProvider>);
         }
 
-        let external_overlay_provider = OpProofsStateProviderRef::new(
-            historical_provider,
-            self.preimage_store.clone(),
-            block_number,
-        );
+        let external_overlay_provider =
+            OpProofsStateProviderRef::new(historical_provider, &self.preimage_store, block_number);
 
         Ok(Box::new(external_overlay_provider))
     }
