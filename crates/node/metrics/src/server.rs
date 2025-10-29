4c431ef37123d3ce2aad3bfe37e9f18c2ae006fd
varun-doshi <doshivarun202@gmail.com>
2025-10-29 13:54:21 +0530

    fix: log full url

--- crates/node/metrics/src/server.rs
+++ crates/node/metrics/src/server.rs
@@ -84,7 +84,7 @@ impl MetricServer {
             .await
             .wrap_err("Could not bind to address")?;
 
-        tracing::info!(target: "reth::cli", "Starting metrics endpoint at {}", listener.local_addr().unwrap().port());
+        tracing::info!(target: "reth::cli", "Starting metrics endpoint at {}", listener.local_addr().unwrap());
 
         task_executor.spawn_with_graceful_shutdown_signal(|mut signal| {
             Box::pin(async move {
