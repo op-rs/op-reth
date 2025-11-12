61c5f8a53d97a7284add61ddb3da2751cfe1e9df
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-11-11 21:19:13 -0400

    Fix conflicts with upstream

--- crates/optimism/trie/tests/live.rs
+++ crates/optimism/trie/tests/live.rs
@@ -145,7 +145,7 @@ fn execute_block<N>(
 ) -> eyre::Result<reth_evm::execute::BlockExecutionOutput<Receipt>>
 where
     N: ProviderNodeTypes<
-            Primitives: FullNodePrimitives<Block = Block, BlockBody = BlockBody, Receipt = Receipt>,
+            Primitives: NodePrimitives<Block = Block, BlockBody = BlockBody, Receipt = Receipt>,
         > + NodeTypesWithDB,
 {
     let provider = provider_factory.provider()?;
@@ -172,7 +172,7 @@ fn commit_block_to_database<N>(
 ) -> eyre::Result<()>
 where
     N: ProviderNodeTypes<
-            Primitives: FullNodePrimitives<Block = Block, BlockBody = BlockBody, Receipt = Receipt>,
+            Primitives: NodePrimitives<Block = Block, BlockBody = BlockBody, Receipt = Receipt>,
         > + NodeTypesWithDB,
 {
     let execution_outcome = ExecutionOutcome {
@@ -210,7 +210,7 @@ async fn run_test_scenario<N>(
 ) -> eyre::Result<()>
 where
     N: ProviderNodeTypes<
-            Primitives: FullNodePrimitives<Block = Block, BlockBody = BlockBody, Receipt = Receipt>,
+            Primitives: NodePrimitives<Block = Block, BlockBody = BlockBody, Receipt = Receipt>,
         > + NodeTypesWithDB,
 {
     let genesis_hash = chain_spec.genesis_hash();
