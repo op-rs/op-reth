563e16fd9f9d98e408d90fd5616994db6ce6866c
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-11-11 21:23:56 -0400

    Fix conflicts with upstream

--- crates/storage/db-api/src/mock.rs
+++ crates/storage/db-api/src/mock.rs
@@ -302,6 +302,14 @@ impl<T: DupSort> DbDupCursorRO<T> for CursorMock {
         Ok(None)
     }
 
+    fn next_dup(&mut self) -> PairResult<T> {
+        Ok(None)
+    }
+
+    fn last_dup(&mut self) -> ValueOnlyResult<T> {
+        Ok(None)
+    }
+
     /// Moves to the next entry with a different key.
     /// **Mock behavior**: Always returns `None`.
     fn next_no_dup(&mut self) -> PairResult<T> {
