f8eee23387e803c75184fa6818b2366e1fb7a27e
itschaindev <jagrutk@protonmail.com>
2025-11-20 20:20:31 +0100

    modify test to work with reth as seq devnet

--- crates/optimism/tests/proofs/core/resyncing_test.go
+++ crates/optimism/tests/proofs/core/resyncing_test.go
@@ -1,10 +1,12 @@
 package core
 
 import (
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
+	"github.com/ethereum-optimism/optimism/op-devstack/dsl"
 	"github.com/ethereum-optimism/optimism/op-devstack/presets"
 	"github.com/ethereum-optimism/optimism/op-e2e/e2eutils/wait"
 	"github.com/ethereum-optimism/optimism/op-service/eth"
@@ -19,6 +21,15 @@ func TestResyncing(gt *testing.T) {
 	ctx := t.Ctx()
 
 	sys := presets.NewSingleChainMultiNode(t)
+	var validatorELNode *dsl.L2ELNode
+	var validatorCLNode *dsl.L2CLNode
+	if strings.Contains(sys.L2EL.ID().Key(), "validator") {
+		validatorELNode = sys.L2EL
+		validatorCLNode = sys.L2CL
+	} else {
+		validatorELNode = sys.L2ELB
+		validatorCLNode = sys.L2CLB
+	}
 
 	alice := sys.FunderL2.NewFundedEOA(eth.OneEther)
 	bob := sys.FunderL2.NewFundedEOA(eth.OneEther)
@@ -28,9 +39,9 @@ func TestResyncing(gt *testing.T) {
 	require.NoError(gt, err)
 	require.Equal(gt, types.ReceiptStatusSuccessful, receipt.Status)
 
-	t.Logf("Stopping L2 CLB and ELB to simulate downtime")
-	sys.L2CLB.Stop()
-	sys.L2ELB.Stop()
+	t.Logf("Stopping validator L2 CL and EL to simulate downtime")
+	validatorELNode.Stop()
+	validatorCLNode.Stop()
 
 	var blockNumbers []uint64
 	// produce some transactions while the node is down
@@ -43,17 +54,17 @@ func TestResyncing(gt *testing.T) {
 	}
 
 	// restart the node and ensure it can sync the missing blocks
-	t.Logf("Restarting L2 CLB and ELB to resync")
-	sys.L2ELB.Start()
-	sys.L2CLB.Start()
+	t.Logf("Restarting validator L2 CL and EL to resync")
+	validatorELNode.Start()
+	validatorCLNode.Start()
 
 	time.Sleep(3 * time.Second)
 
 	err = wait.For(t.Ctx(), 2*time.Second, func() (bool, error) {
-		status := sys.L2CLB.SyncStatus()
+		status := validatorCLNode.SyncStatus()
 		return status.UnsafeL2.Number > blockNumbers[len(blockNumbers)-1], nil
 	})
-	require.NoError(gt, err, "L2 CLB failed to resync to latest block")
+	require.NoError(gt, err, "Validator L2 CL failed to resync to latest block")
 
 	t.Logf("Fetching and verifying proofs for transactions produced while node was down")
 	// verify the proofs for the transactions produced while the node was down
