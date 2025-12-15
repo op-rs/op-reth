8f7404f86d3d16c3cdea9b6c4628cbb99e7ddeda
Arun Dhyani <dhyaniarun7@gmail.com>
2025-12-15 11:41:44 +0530

    lintfixes

--- crates/trie/common/src/hashed_state.rs
+++ crates/trie/common/src/hashed_state.rs
@@ -1557,7 +1557,7 @@ pub mod serde_bincode_compat {
     ///     hashed_state: HashedPostState,
     /// }
     /// ```
-    #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[derive(Debug, Serialize, Deserialize)]
     pub struct HashedPostState<'a> {
         accounts: Cow<'a, B256Map<Option<Account>>>,
         storages: B256Map<HashedStorage<'a>>,
@@ -1576,17 +1576,16 @@ pub mod serde_bincode_compat {
         fn from(value: HashedPostState<'a>) -> Self {
             Self {
                 accounts: value.accounts.into_owned(),
-                storages: value
-                    .storages
-                    .into_iter()
-                    .map(|(k, v)| (k, v.into()))
-                    .collect(),
+                storages: value.storages.into_iter().map(|(k, v)| (k, v.into())).collect(),
             }
         }
     }
 
     impl SerializeAs<super::HashedPostState> for HashedPostState<'_> {
-        fn serialize_as<S>(source: &super::HashedPostState, serializer: S) -> Result<S::Ok, S::Error>
+        fn serialize_as<S>(
+            source: &super::HashedPostState,
+            serializer: S,
+        ) -> Result<S::Ok, S::Error>
         where
             S: Serializer,
         {
@@ -1618,7 +1617,7 @@ pub mod serde_bincode_compat {
     ///     hashed_storage: HashedStorage,
     /// }
     /// ```
-    #[derive(Debug, Clone, Serialize, Deserialize)]
+    #[derive(Debug, Serialize, Deserialize)]
     pub struct HashedStorage<'a> {
         wiped: bool,
         storage: Cow<'a, B256Map<U256>>,
@@ -1657,8 +1656,8 @@ pub mod serde_bincode_compat {
     #[cfg(test)]
     mod tests {
         use crate::{
-            serde_bincode_compat,
             hashed_state::{HashedPostState, HashedStorage},
+            serde_bincode_compat,
         };
         use alloy_primitives::{B256, U256};
         use reth_primitives_traits::Account;
@@ -1684,9 +1683,7 @@ pub mod serde_bincode_compat {
             let decoded: Data = bincode::deserialize(&encoded).unwrap();
             assert_eq!(decoded, data);
 
-            data.hashed_state
-                .storages
-                .insert(B256::random(), HashedStorage::default());
+            data.hashed_state.storages.insert(B256::random(), HashedStorage::default());
             let encoded = bincode::serialize(&data).unwrap();
             let decoded: Data = bincode::deserialize(&encoded).unwrap();
             assert_eq!(decoded, data);
