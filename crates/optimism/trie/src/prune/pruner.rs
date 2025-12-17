90e7c6869ec382098c6f6c4ee700e90b7054ed79
sadiq1971 <sadiqurr8@gmail.com>
2025-12-17 21:06:51 +0600

    bug fix

--- crates/optimism/trie/src/prune/pruner.rs
+++ crates/optimism/trie/src/prune/pruner.rs
@@ -210,7 +210,7 @@ mod tests {
     async fn run_inner_and_and_verify_updated_state() {
         // --- env/store ---
         let dir = TempDir::new().unwrap();
-        let store:OpProofsStorage<Arc<MdbxProofsStorage>> =
+        let store: OpProofsStorage<Arc<MdbxProofsStorage>> =
             OpProofsStorage::from(Arc::new(MdbxProofsStorage::new(dir.path()).expect("env")));
 
         store.set_earliest_block_number(0, B256::ZERO).await.expect("set earliest");
@@ -498,7 +498,7 @@ mod tests {
     #[tokio::test]
     async fn run_inner_where_latest_block_is_none() {
         let dir = TempDir::new().unwrap();
-        let store:OpProofsStorage<Arc<MdbxProofsStorage>> =
+        let store: OpProofsStorage<Arc<MdbxProofsStorage>> =
             OpProofsStorage::from(Arc::new(MdbxProofsStorage::new(dir.path()).expect("env")));
 
         let earliest = store.get_earliest_block_number().await.unwrap();
@@ -519,7 +519,7 @@ mod tests {
         use crate::BlockStateDiff;
 
         let dir = TempDir::new().unwrap();
-        let store:OpProofsStorage<Arc<MdbxProofsStorage>> =
+        let store: OpProofsStorage<Arc<MdbxProofsStorage>> =
             OpProofsStorage::from(Arc::new(MdbxProofsStorage::new(dir.path()).expect("env")));
 
         // Write a single block to set *latest* only.
@@ -545,7 +545,7 @@ mod tests {
         use crate::BlockStateDiff;
 
         let dir = TempDir::new().unwrap();
-        let store:OpProofsStorage<Arc<MdbxProofsStorage>> =
+        let store: OpProofsStorage<Arc<MdbxProofsStorage>> =
             OpProofsStorage::from(Arc::new(MdbxProofsStorage::new(dir.path()).expect("env")));
 
         // Set earliest=4 explicitly
